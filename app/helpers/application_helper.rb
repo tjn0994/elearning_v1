1680794da6fd5e1eaff8a6ba6c597bf185ac5433
Tin <quangtin12b5@gmail.com>
2017-03-12 01:13:37 +0700

    add css static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t "elearning"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
+
+  def current_index page_index , page_size, index
+    (page_index - 1)*page_size + (index + 1)
+  end
 end
