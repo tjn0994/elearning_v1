34c4c325f2d580e892fa69d0acb97f28b274c354
Tin <quangtin12b5@gmail.com>
2017-05-29 22:30:05 +0700

    update data v4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:email, :password, :password_confirmation)
+      user_params.permit(:email, :name, :role, :password, :password_confirmation)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
--- app/models/register_course.rb
+++ app/models/register_course.rb
@@ -4,10 +4,17 @@ class RegisterCourse < ApplicationRecord
 
   validates :date_open, presence: true
   validates :date_close, presence: true
+  validate :invalid_valid, if: ->{date_open.present? && date_close.present?}
 
   enum status: {active: 0, not_active: 1}
 
   # def create_time_register_course
   #   delay(run_at: 1.minutes.from_now).open_time_register_course
   # end
+
+  def invalid_valid
+    if date_open >= date_close
+      errors.add :base, "Ngày kết thúc phải lớn hơn ngày bắt đầu"
+    end
+  end
 end
